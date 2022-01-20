9bf4c7253917a3c37c5fe0cc9c7794befe1f5b65
Ivan MSI <arigatory@gmail.com>
2022-01-20 23:45:19 +0300

    feat: two columns

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
     renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
@@ -9,6 +8,7 @@ createAutoComplete({
 
     },
     onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
         onMovieSelect(movie);
     },
     inputValue(movie) {
@@ -27,6 +27,16 @@ createAutoComplete({
         }
         return response.data.Search;
     }
+};
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete')
+});
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete')
 });
 
 const onMovieSelect = async movie => {
